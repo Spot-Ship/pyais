9dc53dfc8a0840492755b88a50bbd241a19bfe5c
Mark Hardwick <mark@designibl.com>
2023-03-02 13:32:27 +0000

    changing decode type

--- main.py
+++ main.py
@@ -328,20 +328,20 @@ def prep_message_for_timestream(message):
     if message['msg_type'] in [1,2,3,27]:
         try:
             logging.info(f"Status before {message['status']}")
-            message['status'] = message['status'].decode("utf-8")
+            message['status'] = f"{message['status']}"
             logging.info(f"Status after {message['status']}")
         except:
             pass
         if message['msg_type'] in [1,2,3]:
             try:
                 logging.info(f"Maneuver before {message['maneuver']}")
-                message['maneuver'] = message['maneuver'].decode("utf-8")
+                message['maneuver'] = f"{message['maneuver']}"
                 logging.info(f"Maneuver after {message['maneuver']}")
             except:
                 pass
             try:
                 logging.info(f"Turn before {message['turn']}")
-                message['turn'] = float(message['turn'].decode("utf-8"))
+                message['turn'] = float(f"{message['turn']}")
                 if (message['turn']) == -0.0:
                     message['turn'] = 0.0
                 logging.info(f"Turn after {message['turn']}")
